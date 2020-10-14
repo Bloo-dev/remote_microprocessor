86f1a167acda40337a1271bd08feff3a891dcba4
Bloo-dev <contact.bluefire610@gmail.com>
2020-10-14 14:20:34 +0200

    Remove old minified code

--- eeprom.lua
+++ eeprom.lua
@@ -73,8 +73,3 @@ do
 end
 
 sourceFunc(data)
-
-
---[[
-    local a="v1.2"local b,c;local d=error;function error(...)computer.beep(1000,0.5)d(...)end;do local e=component.invoke;local function f(g,h,...)local i=table.pack(pcall(e,g,h,...))if not i[1]then return nil,i[2]else return table.unpack(i,2,i.n)end end;do local j=component.list("screen")()local k=component.list("gpu")()if k and j then f(k,"bind",j)end end;do local l=component.list("modem")()local m=28820;if l then if not select(1,f(l,"isWireless"))then error("The network card must be wireless")end;if not select(1,f(l,"setStrength",256))then error("The network card must be wireless")end;f(l,"open",m)else error("No network card found")end;if f(l,"broadcast",m,"remote_microprocessor:firmware_request",a)then while true do local n,n,n,o,p,q,r,s=computer.pullSignal("modem_message")if m==o then if q=="remote_microprocessor:firmware_response"then local t;b,t=load(r)c=s;if not b then error("Unable to compile recieved code: ",t)end;break end end end else error("Unable to request source from server")end end end;b(c)
-]]
